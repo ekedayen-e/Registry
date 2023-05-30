56754377abbb0433d163202bc8969f7d19153442
erubae <rombuski@gmail.com>
2023-05-30 05:54:39 +0100

    add client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Book from './components/Book'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Book />
+    </div>
+  );
+}
+
+export default App;
